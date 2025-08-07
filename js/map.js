40487097c4f4031749bd6a01565e886c67232a9d
Xavier Medrano <xaviermedrano@Xaviers-MacBook-Pro.local>
2025-08-07 15:19:21 -0400

    adjust zoom

--- js/map.js
+++ js/map.js
@@ -6,8 +6,7 @@ const map = new mapbox.Map({
   container: "map",
   style: "mapbox://styles/housingstudies/cmcb0c6ql002001rz02zqewvm",
   center: [-87.66231, 41.85754], // [lng, lat]
-  zoom: 13, // parcels will not be visible below 13
-  minZoom: 13,
+  zoom: 9, // parcels will not be visible below 9
 })
 
 // Zoom and rotation controls
